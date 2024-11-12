a1e0192944e38254d1468bc378fbc4feb6307ee7
Printed-chololate <leonard.zhengzekai@gmail>
2024-11-12 19:39:53 +0800

    begin

---
+++ data.c
@@ -0,0 +1,46 @@
+ker=1-recalculate kernel;ker=0-load from files:
+ker= 1
+new=1-started from initial values;new=0-continue computation:
+new= 0
+boundary=1(xyz-Neumann boundary);2(xy-periodic,z_Neuman);3(Thomas Method)
+boundary= 1
+Number of points along x-axis:
+M= 128
+Number of points along y-axis:
+N= 128
+Number of points along z-axis:
+K= 2
+Number of kernel summation(set 0 if no periodic boundary):
+nsum= 0
+Number of time steps:
+nsteps= 15.0e3
+Number of ouput points:
+npoints= 600
+Time step size:
+dt= 3.0e-14
+x-length of sample:
+X= 128e-9
+y-length of sample:
+Y= 128e-9
+Thickness of sample:
+Z= 2e-9
+Sature magnetization:
+Ms= 580e3
+Exchange energy term:
+A= 15e-12
+Anisotropy term:
+Ku= 0.8e6
+Gyromagnetic ratio:
+gamma= 1.2866e11
+Damping factor:
+alpha= 0.8
+Relative tolerance:
+RelTol= 1.0e-9
+driving current density:
+dcd= 2.1e6
+spin Hall angle:
+sha= 0.1
+radius of the initial perturbation:
+rad= 4e-9
+interface-induced DMI:
+idmi= 2.7e-3
